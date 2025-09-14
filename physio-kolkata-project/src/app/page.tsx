fb4116a72eae4f017eabcbe2365a62533361de0d
Arpan Datta <arpandatta72@gmail.com>
2025-09-14 09:59:20 +0530

    section added for who are you

--- physio-kolkata-project/src/app/page.tsx
+++ physio-kolkata-project/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import Example from './header/header'
+import Example from "./header/header";
+import WhoAreWe from "./WhoAreWe";
 export default function Home() {
   return (
     <div className="">
-      <Example/>
+      <Example />
+      <WhoAreWe />
     </div>
   );
 }
