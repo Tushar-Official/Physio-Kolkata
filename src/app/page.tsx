77d9636ddc5a574c5f9073ac8a4d28baa5d9311f
Tushar Kumar <73745126+Tushar-Official@users.noreply.github.com>
2025-10-13 01:11:09 +0530

    Adding global analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,26 @@ import OurLocation from "./OurLocation";
 import OurSpecialists from "./OurSpecialists";
 import Partner from "./Partner";
 import ReviewCarouselHome from "./Reviews/index";
+import Script from "next/script";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col">
+    <div className="flex flex-col">
+      {/* Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-B58259VLT2"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-B58259VLT2');
+        `}
+      </Script>
+
+      {/* Your page content */}
       <Header />
       <TopSectionContent />
       <ServicesCardSection />
